66bc88575c0053c0b4665adf461ba963346fd70d
Doug-Silva <silvatec.br@gmail.com>
2026-03-22 22:13:52 -0300

    HashMap e TreeMap

---
+++ src/EstudoHashMap.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class EstudoHashMap {
+    public static void main(String[] args) {
+        HashMap<String, Double> notasAlunos = new HashMap<>();
+        notasAlunos.put("Alice", 10.5);
+        notasAlunos.put("Matheus", 8.0);
+        notasAlunos.put("Maria", 9.9);
+
+        for (String nome : notasAlunos.keySet()){
+            double nota = notasAlunos.get(nome);
+            System.out.println(nome + " : " + nota);
+
+        }
+    }
+}
