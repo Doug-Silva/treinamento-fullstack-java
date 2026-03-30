6fabfe3de322473151506c37518693fd7027d171
Doug-Silva <silvatec.br@gmail.com>
2026-03-29 21:24:34 -0300

    Pilares do POO: Composição e Agregação

---
+++ src/associacao/Carro.java
@@ -0,0 +1,27 @@
+package associacao;
+
+public class Carro {
+
+    //atributo do tipo Motor
+    private Motor motor;
+
+    public Carro(Motor motor) {
+        this.motor = motor;
+    }
+
+    public Carro(int cilindradas) {
+        this.motor = new Motor(cilindradas);
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public void mostrarCilindradas() {
+        System.out.println("Carro com motor de: " + motor.getCilindradas() + " cilindradas");
+    }
+}
---
+++ src/associacao/Principal.java
@@ -0,0 +1,26 @@
+package associacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro fusca = new Carro(20);
+        fusca.mostrarCilindradas();
+
+        Funcionarios joao = new Funcionarios("João");
+        Funcionarios joana = new Funcionarios("Joana");
+        Funcionarios matheus = new Funcionarios("Matheus");
+        List<Funcionarios> listaDeFuncionarios = new ArrayList<>();
+        listaDeFuncionarios.add(joao);
+        listaDeFuncionarios.add(joana);
+        listaDeFuncionarios.add(matheus);
+
+        Departamento ti = new Departamento("TI", listaDeFuncionarios);
+        ti.mostrarFuncionarios();
+
+        Departamento marketing = new Departamento("Marketing", Collections.emptyList());
+        marketing.mostrarFuncionarios();
+    }
+}
