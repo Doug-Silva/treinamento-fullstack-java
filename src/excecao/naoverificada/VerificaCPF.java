b712cad336dd6833f77bd607feb7e3b37c7566c4
Doug-Silva <silvatec.br@gmail.com>
2026-04-01 21:47:38 -0300

    Exceções Não Verifics

---
+++ src/excecao/naoverificada/VerificaCPF.java
@@ -0,0 +1,25 @@
+package excecao.naoverificada;
+
+import java.util.Scanner;
+
+public class VerificaCPF {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu CPF ");
+        String verificaCPF = verificaCPF(scanner.nextLine());
+        System.out.println(verificaCPF);
+    }
+
+    public static String verificaCPF(String cpf){
+        try {
+            if (cpf.equals("0001234500045")){
+                throw new RuntimeException("CPF já cadastrado" + cpf);
+            }else {
+                return "CPF válido, usuário cadastrado";
+            }
+        }catch (RuntimeException e){
+            throw new RuntimeException("CPF já cadastrado" + cpf);
+        }
+    }
+}
